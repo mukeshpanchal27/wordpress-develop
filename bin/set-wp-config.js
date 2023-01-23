430d9d2f4943904d04270a9d9c61a26026bfb318
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-01-23 18:06:31 +0530

    Update

--- bin/set-wp-config.js
+++ bin/set-wp-config.js
@@ -15,14 +15,14 @@ fs.mkdirSync( baseDirectory, { recursive: true } );
 const rootDirectory = getRandomTemporaryPath();
 const performanceTestDirectory = rootDirectory + '/tests';
 runShellScript( 'mkdir -p ' + rootDirectory );
-await runShellScript(
+runShellScript(
     'cp -R ' + baseDirectory + ' ' + performanceTestDirectory
 );
 
 log( '    >> Installing dependencies and building packages' );
-await runShellScript(
+runShellScript(
     'npm ci && node ./bin/packages/build.js',
     performanceTestDirectory
 );
 log( '    >> Creating the environment folders' );
-await runShellScript( 'mkdir -p ' + rootDirectory + '/envs' );
+runShellScript( 'mkdir -p ' + rootDirectory + '/envs' );
