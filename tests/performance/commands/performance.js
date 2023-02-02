5a37e1b513635e83e35acd07512e06e57e9e16a7
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-02-02 14:40:55 +0530

    Update

--- tests/performance/commands/performance.js
+++ tests/performance/commands/performance.js
@@ -387,7 +387,7 @@ async function runPerformanceTests( branches, options ) {
 			rawResults[ i ] = {};
 			for ( const branch of branches ) {
 				// @ts-ignore
-				const environmentDirectory = branchDirectories[ branch ];
+                const environmentDirectory = branchDirectories[ branch ] + '/plugin';
 				log( `    >> Branch: ${ branch }, Suite: ${ testSuite }` );
 				log( '        >> Starting the environment.' );
 				await runShellScript(
