38059d1d039d572e5980b9d586e78bdb580a9cdf
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-02-11 15:26:34 +0530

    Update

--- tests/performance/specs/front-end-classic-theme.test.js
+++ tests/performance/specs/front-end-classic-theme.test.js
@@ -7,14 +7,18 @@ import { writeFileSync } from 'fs';
 /**
  * WordPress dependencies
  */
-import { createURL, logout } from '@wordpress/e2e-test-utils';
+import { activateTheme, createURL } from '@wordpress/e2e-test-utils';
 
-describe( 'Front End Performance', () => {
+describe( 'Server Timing – Twenty Twenty One', () => {
 	const results = {
 		wpBeforeTemplate: [],
 		wpTemplate: [],
 		wpTotal: [],
 	};
+
+	beforeAll( async () => {
+		await activateTheme( 'twentytwentyone' );
+	} );
 
 	afterAll( async () => {
 		const resultsFilename = basename( __filename, '.js' ) + '.results.json';
