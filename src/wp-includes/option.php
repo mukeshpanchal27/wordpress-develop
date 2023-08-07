02f1d5119665511a5564bfbee5b65fe13128085e
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-08-07 17:35:29 +0530

    Update option.php

--- src/wp-includes/option.php
+++ src/wp-includes/option.php
@@ -271,11 +271,11 @@ function prime_options( $options ) {
 		$option_names = implode( "','", array_map( 'esc_sql', $options_to_prime ) );
 		$query   = "SELECT option_name, option_value FROM {$wpdb->options} WHERE option_name IN ('$option_names')";
 		$results = $wpdb->get_results( $query, OBJECT_K );
-		
+
 		foreach ( $results as $result ) {
 			$alloptions[ $result->option_name ] = maybe_unserialize( $result->option_value );
 		}
-		
+
 		wp_cache_set( 'alloptions', $alloptions, 'options' );
 	}
 }
