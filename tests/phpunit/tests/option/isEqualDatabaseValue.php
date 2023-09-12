511d5f09fbd0b424f4463c1e66292115558568bd
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-09-12 12:32:23 +0530

    Update isEqualDatabaseValue.php

--- tests/phpunit/tests/option/isEqualDatabaseValue.php
+++ tests/phpunit/tests/option/isEqualDatabaseValue.php
@@ -19,6 +19,11 @@ class Tests_Is_Equal_Database_Value extends WP_UnitTestCase {
 		$this->assertEquals( $expected, is_equal_database_value( $old_value, $new_value ) );
 	}
 
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
 	public function data_is_equal_database_value() {
 		return array(
 			// Equal values.
@@ -27,13 +32,38 @@ class Tests_Is_Equal_Database_Value extends WP_UnitTestCase {
 			// Not equal values.
 			array( '123', '456', false ),
 
+			// Truthy.
+			array( 1, '1', true ),
+			array( 1.0, '1', true ),
+			array( '1', '1', true ),
+			array( true, '1', true ),
+			array( '1.0', '1', false ),
+			array( '    ', '1', false ),
+			array( array( '0' ), '1', false ),
+			array( new stdClass(), '1', false ),
+			array( 'Howdy, admin!', '1', false ),
+
 			// False-ish values and empty strings.
-			array( false, '0', true ),
+			array( 0, '0', true ),
+			array( 0.0, '0', true ),
+			array( '0', '0', true ),
 			array( '', '0', true ),
+			array( false, '0', true ),
+			array( null, '0', false ),
+			array( array(), '0', false ),
+
+			// Object values.
+			array( (object) array( 'foo' => 'bar' ), (object) array( 'foo' => 'bar' ), true ),
+			array( (object) array( 'foo' => 'bar' ), (object) array( 'foo' => 'baz' ), false ),
+			array( (object) array( 'foo' => 'bar' ), serialize( (object) array( 'foo' => 'bar' ) ), false ),
+			array( serialize( (object) array( 'foo' => 'bar' ) ), (object) array( 'foo' => 'bar'), false ),
+			array( serialize( (object) array( 'foo' => 'bar' ) ), (object) array( 'foo' => 'baz' ), false ),
 
 			// Serialized values.
-			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'bar' ) ), true ),
-			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'baz' ) ), false ),
+			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'bar' ) ), false ),
+			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'baz') ), false ),
+			array( serialize( (object) array( 'foo' => 'bar' ) ), serialize( (object) array( 'foo' => 'bar') ), true ),
+			array( serialize( (object) array( 'foo' => 'bar' ) ), serialize( (object) array( 'foo' => 'baz') ), false ),
 		);
 	}
 }
