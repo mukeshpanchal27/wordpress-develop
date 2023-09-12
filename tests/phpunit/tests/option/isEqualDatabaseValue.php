7b160818a7285a2c60c1ef4f4bd7e17344ce61eb
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-09-12 11:25:21 +0530

    Apply suggestions from code review

--- tests/phpunit/tests/option/isEqualDatabaseValue.php
+++ tests/phpunit/tests/option/isEqualDatabaseValue.php
@@ -33,7 +33,7 @@ class Tests_Is_Equal_Database_Value extends WP_UnitTestCase {
 
 			// Serialized values.
 			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'bar' ) ), true ),
-			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'baz') ), false ),
+			array( array( 'foo' => 'bar' ), serialize( array( 'foo' => 'baz' ) ), false ),
 		);
 	}
 }
