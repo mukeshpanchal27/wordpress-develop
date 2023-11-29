9427655e95e3a6e83bd74b111c711e257fefd3c9
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-11-29 11:27:45 +0530

    Update upgrade.php

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -2379,6 +2379,9 @@ function upgrade_650() {
 
 		$autoload = array_fill_keys( $theme_mods_options, 'no' );
 		wp_set_option_autoload_values( $autoload );
+
+		// Reload autoload options.
+		wp_load_alloptions();
 	}
 }
 
